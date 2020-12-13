445e95659087bbb86e945d160f29dd512b6d065d
Trever Lyman <lymantj@gmail.com>
2020-12-12 19:26:31 -0700

    adding final work to get the multiple face detection function working

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  // console.log(boxes); 
+  var keyProp = 0; 
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        { boxes.map(box => {
+          keyProp += 1;
+          return <div key={keyProp} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        })}
       </div>
     </div>
   );
